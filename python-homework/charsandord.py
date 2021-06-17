440573b45b9bacaf85bb54b0772dd8cb44874dfe
nail11 <nail@t-online.hu>
2021-06-17 14:52:06 +0200

    Javított verzió - chr() használva, de hosszú magánhangzók nélkül

--- python-homework/charsandord.py
+++ python-homework/charsandord.py
@@ -1,10 +1,12 @@
-kisbetuk = ['a','á','b','c','d','e','f','g','h','i','í','j','k','l','m','n','o','ó','ö','ő','p','q','r','s','t','u','ú','ü','ű','x','y','v','w','z']
+kisbetuk = []
+
+for i in range(26):
+    kisbetuk.append(chr(i+97))
 
 k = len(kisbetuk) % 10
 
 for i in range(k):
-    print(kisbetuk[i], ord(kisbetuk[i]), kisbetuk[i+10], ord(kisbetuk[i+10]), kisbetuk[i+20], ord(kisbetuk[i+20]), kisbetuk[i+30], ord(kisbetuk[i+30]))
-
+    print(kisbetuk[i], ord(kisbetuk[i]), kisbetuk[i+10], ord(kisbetuk[i+10]), kisbetuk[i+20], ord(kisbetuk[i+20]))
 for i in range(10-k):
-    print(kisbetuk[i+k], ord(kisbetuk[i+k]), kisbetuk[i+k+10], ord(kisbetuk[i+k+10]), kisbetuk[i+k+20], ord(kisbetuk[i+k+20]))
+    print(kisbetuk[i+k], ord(kisbetuk[i+k]), kisbetuk[i+k+10], ord(kisbetuk[i+k+10]))
 
