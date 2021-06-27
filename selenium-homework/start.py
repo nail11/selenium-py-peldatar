6b32d8885c5a6442dcc4b51658f00e96780a25c0
Nagy László <nail@t-online.hu>
2021-06-27 21:21:25 +0200

    Update start.py

--- selenium-homework/start.py
+++ selenium-homework/start.py
@@ -1,18 +1,16 @@
 from selenium import webdriver
 
-# from selenium.webdriver.chrome.options import Options
-
-# from webdriver_manager import ChromeDriverManager
-# ha a Chrome éa a chromedriver verzió illeszkedés kétséges - alapvetően mindíg
-
-# options = Options()
-# options.add_argument("--headless")
-# options.add_argument('--disable-gpu')
-
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-# ha a Chrome éa a chromedriver verzió illeszkedés kétséges - alapvetően mindíg
-
 driver = webdriver.Chrome()
 
 driver.get("https://www.google.com")
-# 'Google'
+
+# from selenium.webdriver.chrome.options import Options
+# from webdriver_manager import ChromeDriverManager
+# ha a Chrome éa a chromedriver verzió illeszkedés kétséges - alapvetően mindíg
+# options = Options()
+# options.add_argument("--headless")
+# options.add_argument('--disable-gpu')
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# ha a Chrome éa a chromedriver verzió illeszkedés kétséges - alapvetően mindíg
+
+
