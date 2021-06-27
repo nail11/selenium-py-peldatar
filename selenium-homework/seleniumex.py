ece167120b9cc71f02a6cb44f720ca53578f5dee
nail11 <nail@t-online.hu>
2021-06-27 07:04:32 +0200

    működő seleniumex.py file és funkció

---
+++ selenium-homework/seleniumex.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.python.org")
+
+try:
+    find_element_by_id("nemletezik")
+
+except:
+    print("A 'nemlétezik' azonosítójú elem nem létezik")
+
+finally:
+    driver.close()
+
+# 'nemlétező elem' hibát kezelő függvény a seleniumex_fun.py file-ben
+
+
+
---
+++ selenium-homework/seleniumex_fun.py
@@ -0,0 +1,24 @@
+def find_element(site, id):
+
+    # site = input('Mi a keresett azonosító? ', site)
+    # id = input('Mi a keresett azonosító? ', id)
+
+    from selenium import webdriver
+
+    driver = webdriver.Chrome()
+
+    driver.get(site)
+
+    try:
+        find_element_by_id(str(id))
+
+    except:
+        print("A" + "'" + id + "'" + "azonosítójú elem nem létezik")
+
+    finally:
+        driver.close()
+
+site = input('Mi az url? ')
+id = input('Mi a keresett azonosító? ')
+
+find_element(site, id)
