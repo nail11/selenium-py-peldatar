a158ab82f2fa58332d295b59930a8b8971fe2861
nail11 <nail@t-online.hu>
2021-06-29 20:46:58 +0200

    start,py ismét müködik

--- selenium-homework/start.py
+++ selenium-homework/start.py
@@ -1,18 +1,12 @@
 from selenium import webdriver
 
-# from selenium.webdriver.chrome.options import Options
-
-# from webdriver_manager import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 # ha a Chrome éa a chromedriver verzió illeszkedés kétséges - alapvetően mindíg
 
-# options = Options()
-# options.add_argument("--headless")
-# options.add_argument('--disable-gpu')
-
-# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install())
 # ha a Chrome éa a chromedriver verzió illeszkedés kétséges - alapvetően mindíg
 
 driver = webdriver.Chrome()
 
 driver.get("https://www.google.com")
-# 'Google'
+
