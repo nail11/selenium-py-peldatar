ff6e211d2b981a2ae92244660e8c672f9d734a29
Nagy László <nail@t-online.hu>
2021-07-09 22:21:26 +0200

    fordító OK

--- python2-homework/fordito.py
+++ python2-homework/fordito.py
@@ -3,29 +3,31 @@
 def num_turn():
     x = 1
     list = []
-    while x > 0:
 
-        x = int(input('Adj meg egy egész számot: '))
-        if x > 0:
+    # x = int(input('Adj meg egy nullától küklönböző egész számot: '))
+
+    # if isinstance(x, (int)):
+
+    while int(x) > 0:
+        x = input('Adj meg egy nullától küklönböző egész számot: ')
+
+        if int(x) > 0:
 
             list.append(x)
 
-        else:
+        elif int(x) == 0:
 
             print('A megadott szám 0, befejezem a számok bekérését !')
 
             print('A számlista: ')
+
             print(list)
 
             print('A fordított számlista:')
 
             print(list[::-1])
 
-    return
 
 # a fordító függvény meghívása
 
 num_turn()
-
-
-
--- testproject/webdriver_test.py
+++ testproject/webdriver_test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
+driver = webdriver.Edge()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
