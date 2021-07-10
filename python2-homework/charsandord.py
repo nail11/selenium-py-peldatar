246f5cd3ed708178adc831a850ab8a7eb25b43de
Nagy László <nail@t-online.hu>
2021-07-10 18:19:20 +0200

    fordító OK, charsandord OK

---
+++ python2-homework/charsandord.py
@@ -0,0 +1,32 @@
+# A feladatokat külön python fileban oldd meg. Minden feladat tartalmazza az elvárt filenevet. Ezen a néven fogadható el a megoldás.
+#
+# Szavak előfordulási gyakorisága
+# Állapítsuk meg, hogy az alábbi szövegben (TEXT) melyik szó hányszor fordul elő s írassuk ki az eredményt a következő formában: szó_1 előfordulások_száma_1 szó_2 előfordulások_száma_2 ...
+#
+# Az eredményt úgy írassuk ki, hogy a lista szavak szerint rendezve legyen. Minden szó kisbetűsen szerepeljen, vagyis pl. a 'The' és 'the' szavak azonos szónak számítanak.
+#
+# Használjuk az str.split() függvényt (paraméter nélkül) a whitespace karakterek eltávolítására.
+#
+# Az egyes szavakhoz kapcsolódó írásjelekkel (pont, vessző, idézőjel, stb.) nem kell most foglalkozni.
+#
+# Ezt a szöveget használd a ruttatáshoz: The Old Sea-dog at the Admiral Benbow
+
+
+with open('old_see_dog.txt') as text:
+    content = text.read()
+
+    word_list = content.lower().replace('\n', '').replace('\r', '').replace('"', '').replace('.', '').replace(',', '') \
+ \
+        .replace(';', '').replace('?', '').replace('--', '').replace('17__', '').replace(':', '').split()
+
+word = ''
+db = 0
+word_dic = dict()
+
+for i, word in enumerate(word_list):
+
+    if word not in word_dic:
+        word_dic.update({word: word_list.count(word)})
+        sorted_word_dic = dict(sorted(word_dic.items()))
+
+print(sorted_word_dic)
