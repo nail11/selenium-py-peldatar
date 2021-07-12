2278872571ef0f148dd772cd990d1ba9d201a7f7
nail11 <nail@t-online.hu>
2021-07-12 16:45:28 +0200

    minesweeper_sel.py munka alatt

--- selenium2-homework/minesweeper_sel.py
+++ selenium2-homework/minesweeper_sel.py
@@ -5,4 +5,29 @@
 # az http://localhost:9999/minesweeper-game.html oldalt. A feladat egyszerű éld túl az aknamezőt.
 # A selenium python programnak kell automatikusan végigjétszania a játékot. A megoldáshoz használhattsz más
 # könyvtárakat is pl: https://github.com/madewokherd/mines
+from selenium import webdriver
 
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver = webdriver.Chrome()
+
+driver.get("http://localhost:9999/minesweeper-game.html")
+
+# mine = driver.find_elements_by_class_name('covered')
+# mine = driver.find_elements_by_class_name('mines1')
+covered = driver.find_elements_by_css_selector('div.covered')
+
+for i in range(len(covered)):
+    covered[i].click()
+    covered = driver.find_elements_by_css_selector('div.covered')
+    if covered[i].get_attribute('div') == 'mine':
+            print(covered[i].get_attribute('div'))
+            print('A játéknak vége !')
+    else:
+            i += 1
+print(type(covered))
+# mine.click()
+print(len(covered))
+# print(mine.get_attribute('class'))
