f338168122e0a0aa70a7cfddb1a22a5b96a4e1a3
nail11 <nail@t-online.hu>
2021-07-12 10:36:45 +0200

    selenium python2 gyakorlás

---
+++ selenium2-homework/minesweeper_sel.py
@@ -0,0 +1,7 @@
+# Egy kis játékra invitállak. Feladatod, hogy selenium segítségével megnyerd az aknakereső játékot.
+#
+# Készíts egy Python alkalmazást ami selenium-ot használ. Készíts egy Python alkalmazást ami selenium-ot
+# használ. Indítsd el lokálisan a selenium-py-peldatar alkalmazást. A program töltse be a példatárból
+# az http://localhost:9999/minesweeper-game.html oldalt. A feladat egyszerű éld túl az aknamezőt.
+# A selenium python programnak kell automatikusan végigjétszania a játékot. A megoldáshoz használhattsz más
+# könyvtárakat is pl: https://github.com/madewokherd/mines
