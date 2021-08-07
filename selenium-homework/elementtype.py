9d28861e2a68cacaed61c7c315526c49588b2cee
nail11 <nail@t-online.hu>
2021-08-07 16:22:51 +0200

    gyakorlás

--- selenium-homework/elementtype.py
+++ selenium-homework/elementtype.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,6 +10,16 @@ driver.get("http://localhost:9999/trickyelements.html")
 
 for i in range(1, 6):
 
-   element_list = (driver.find_element_by_id( 'element'+str(i)))
+   #element_list = (driver.find_element_by_id( 'element'+str(i)))
 
-print(element_list)
+   element = driver.find_element_by_tag_name("button")
+   element.click()
+
+   el_id = element.get_attribute('id')
+   el_text = element.get_attribute("onclick")
+   l = el_text[8:16]
+
+   assert el_id == l
+print(f"A szöveg - '{l}"+" was clicked'- tartalmazza a gomb azonosítóját !" )
+time.sleep(1)
+driver.close()
